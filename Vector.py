e09a73661cf015f2a9a2732cb9a49dfba72d93cb
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2022-12-01 17:27:14 +0100

    remove vector3

--- Vector.py
+++ Vector.py
@@ -1,47 +1,5 @@
 from math import sqrt, pow
 from constants import *
-class Vector3:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def magnitude(self):
-        return sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2) )
-
-    def __add__(self, b):
-        if type(b) is Vector3:
-            return Vector3(self.x + b.x, self.y + b.y, self.z + b.z)
-        elif type(b) is Vector2:
-            return Vector3(self.x + b.x, self.y + b.x, self.z)
-        else:
-            return Vector3(self.x + b, self.y + b, self.z + b)
-
-    def __sub__(self, b):
-        if type(b) is Vector3:
-            return Vector3(self.x - b.x, self.y - b.y, self.z - b.z)
-        elif type(b) is Vector2:
-            return Vector3(self.x - b.x, self.y - b.y, self.z)
-        else:
-            return Vector3(self.x - b, self.y - b, self.z - b)
-
-    def __mul__(self, b):
-        if type(b) is Vector3:
-            return Vector3(self.x * b.x, self.y * b.y, self.z * b.z)
-        return Vector3(self.x * b, self.y * b, self.z * b)
-
-    def __truediv__(self, b):
-        if type(b) is Vector3:
-            return Vector3(self.x / b.x, self.y / b.y, self.z / b.z)
-        return Vector3(self.x / b, self.y / b, self.z / b)
-
-
-    def toMatrix(self):
-        return [[self.x], [self.y], [self.z]]
-
-
-    def __repr__(self):
-        return f'{self.x} , {self.y}, {self.z}'
 
 class Vector2:
     def __init__(self, x, y):
